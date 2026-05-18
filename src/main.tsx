bf9482c7427e860b9535a828867135f928a38c9a
Alef <aleftorresbr@gmail.com>
2026-05-18 14:05:30 -0300

    Construindo os primeiros componentes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
+import { Heading } from './components/Heading'
+import App from './App'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Heading />
   </StrictMode>,
 )
