58bcec5759c1da370a22d97a7b1677fd0edaaca0
Alef <aleftorresbr@gmail.com>
2026-06-03 13:06:29 -0300

    Adicionando novas paginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './styles/theme.css'
 import './styles/global.css'
-import Home from './pages/Home'
+import AboutPomodoro from './pages/AboutPomodoro'
 
 export default function App() {
-  return (<Home />)
+  return (<AboutPomodoro />)
 }
